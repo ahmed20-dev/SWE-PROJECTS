3532cc5ced676d970db0f7fe175401e8db5fb11c
Ahmedckarim <252542926+Ahmedckarim@users.noreply.github.com>
2026-09-24 00:15:50 +0300

    update

--- 1. Python/Python-Projects/P08 To-Do List V2/app/manager.py
+++ 1. Python/Python-Projects/P08 To-Do List V2/app/manager.py
@@ -2,17 +2,19 @@ from models import Task
 from validators import Validators
 
 class Manager:
+    def __init__(self):
+        self.expenses = []
 
-    def view_task(self, tasks):
+    def view_task(self):
         pass
 
-    def add_task(self, tasks):
+    def add_task(self):
         pass
-    def update_task(self, tasks):
+    def update_task(self):
         pass
-    def delete_task(self, tasks):
+    def delete_task(self):
         pass
-    def mark_task(self, tasks):
+    def mark_task(self):
         pass
 
 
