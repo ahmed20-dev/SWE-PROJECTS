b2eac2941dc6dd62c393bda1bb3ffd1cb903884e
Ahmedckarim <252542926+Ahmedckarim@users.noreply.github.com>
2026-09-24 19:48:06 +0300

    validating user input for P08

--- 1. Python/Python-Projects/P08 To-Do List V2/app/validators.py
+++ 1. Python/Python-Projects/P08 To-Do List V2/app/validators.py
@@ -1,12 +1,24 @@
 
 class Validator:
    
-   def validate_id(self):
-      pass
-   def validate_title(self):
-      pass
-   def validate_description(self):
-        pass
+   def validate_id(self, id, tasks):
+      try: 
+          id = int(id)
+
+          if any(task["id"] == id for task in tasks):
+            print("This Id already exist.")
+            return id
+      except ValueError as e:
+          print(e)
+          return id
+                   
+   def validate_title(self,title, tasks):
+      if not id:
+          print("Title can't be empty.")
+          return
+      if any(task["title"] == title for task in tasks):
+          print('This Task already exist')
+        
    def validate_priority(self):
        pass
    def validate_date(self):
